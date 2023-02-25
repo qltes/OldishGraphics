8fd4c1a8b823bbe5503f49735e9c43dec283c381
theez <theez@DESKTOP-5KND8S1>
2023-02-25 13:52:43 -0330

    Add project files.

---
+++ OldishGraphics/Class1.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+using UnityEngine;
+
+[BepInPlugin("com.qltesinc.OldishGraphics", "Texture Mod", "1.0.0")]
+public class OldishGraphics : BaseUnityPlugin
+{
+    void Awake()
+    {
+        QualitySettings.masterTextureLimit = 1;
+        UnityEngine.Debug.Log("Graphics Changed");
+    }
+}
